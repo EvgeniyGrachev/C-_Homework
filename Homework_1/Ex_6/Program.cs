2d884283e5c641ab76f602e01341e25c74cd63a8
Evgeniy <zzz12@list.ru>
2022-12-18 17:50:08 +0300

    Homework_1

---
+++ Homework_1/Ex_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int number = int.Parse(userInput);
+
+if (number % 2 == 0) 
+{
+    Console.WriteLine($"{number} -> чётное");
+}
+else
+{
+    Console.WriteLine($"{number} -> нечётное");;
+}
