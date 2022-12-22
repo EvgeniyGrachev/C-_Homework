b90e7d6f0a3bb1fa15cf53f64ba0d59a47bc8d0f
Evgeniy <zzz12@list.ru>
2022-12-22 15:29:25 +0300

    Homework_2

---
+++ Homework_2/Ex_10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трёхзначное целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int num = int.Parse(userInput);
+
+if (num >= 100 && num < 1000)
+{
+    int a = num / 10 % 10;
+    Console.WriteLine($"{num} -> {a}");
+}
+else
+{
+    Console.WriteLine("Введёно не трёхзначное целое число");
+}
---
+++ Homework_2/Ex_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int num = int.Parse(userInput);
+Console.Write($"{num}");
+
+if (num < 100)
+{
+    Console.Write($" -> третьей цифры нет");
+}
+else
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    int a = num % 10;
+    Console.Write($" -> {a}");
+}
