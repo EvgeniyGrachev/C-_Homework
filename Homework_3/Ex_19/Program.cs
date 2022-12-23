7475396adc9219f51eb84ee1f075e39a7797e591
Evgeniy <zzz12@list.ru>
2022-12-23 17:54:32 +0300

    Homework_3

---
+++ Homework_3/Ex_19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное цклое число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Введено не пятизначное целое число");
+}
+else if (num /10000 % 10 == num % 10 && num /1000 % 10 == num /10 % 10)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else
+{
+    Console.WriteLine($"{num} -> нет");
+}
---
+++ Homework_3/Ex_23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите целое положительное число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+int index = 1;
+Console.Write($"{num} ->");
+
+if (num < 1)
+{
+    Console.Write(" Введено не целое положительное число");
+}
+
+while (index <= num)
+{
+    double cube = Math.Pow(index, 3);
+    Console.Write($" {cube},");
+    index++;
+}
