662ea9dd90d1c23f23d68bc57f959372aeba45b4
Evgeniy <zzz12@list.ru>
2023-01-18 23:44:28 +0300

    Homework_4

---
+++ Homework_4/Ex_25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+int numA = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int numB = GetNumberFromUser("Введите целое число B: ", "Ошибка ввода!");
+double degreeNumbers = GetDegreeNumbers(numA, numB);
+Console.WriteLine($"{numA}, {numB} -> {degreeNumbers}");
+
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+// Возводит число А в степень В
+double GetDegreeNumbers(int numberA, int numberB)
+{
+    double degree = Math.Pow(numA, numB);
+    return degree;
+}
---
+++ Homework_4/Ex_27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+// Вычисляет сумму цифр в числе
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
