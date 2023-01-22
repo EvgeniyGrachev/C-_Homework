bc88015b85eead54e1e8f3e4abd02f16b5b2bf67
Evgeniy <zzz12@list.ru>
2023-01-22 20:25:34 +0300

    Homework_5

---
+++ Homework_5/Ex_34/Program.cs
@@ -0,0 +1,33 @@
+﻿//34 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] array = GetArray(6, 100, 1000);
+int evenNumbers = GetEvenNumbers(array);
+Console.WriteLine($"[{String.Join(" ", array)}] -> {evenNumbers}");
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+// Возвращает количество чётных чисел массива arr
+
+int GetEvenNumbers(int[] arr)
+{
+    int evenNum = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) evenNum ++;
+    }
+    return evenNum;
+}
---
+++ Homework_5/Ex_38/Program.cs
@@ -0,0 +1,52 @@
+﻿//38 Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
+
+Console.Clear();
+int[] array = GetArray(6, -100, 101);
+int maxElement = GetMaxElement(array);
+int minElement = GetMinElement(array);
+Console.WriteLine($"[{String.Join(" ", array)}] -> {maxElement - minElement}");
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+// Возвращает максимальный элемент массива arr
+
+int GetMaxElement(int[] arr)
+{
+    int maxEl = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maxEl)
+        {
+            maxEl = arr[i];
+        }
+    }
+    return maxEl;
+}
+
+// Возвращает минимальный элемент массива arr
+
+int GetMinElement(int[] arr)
+{
+    int minEl = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < minEl)
+        {
+            minEl = arr[i];
+        }
+    }
+    return minEl;
+}
