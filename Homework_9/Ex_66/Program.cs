08be5f9c879e1b882ee480b80e921390a1cfd580
Evgeniy <zzz12@list.ru>
2023-02-05 16:18:03 +0300

    Homework_9

---
+++ Homework_9/Ex_66/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+/////////////////////////////////////////////////////////////
+
+Console.Clear();
+
+Console.Write("Введите первое целое положительное число: ");
+int oneNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое положительное число: ");
+int twoNumber = int.Parse(Console.ReadLine() ?? "");
+
+int sum = GetSumNumbers(oneNumber, twoNumber);
+Console.WriteLine($"{oneNumber} - {twoNumber} -> {sum}");
+
+/////////////////////////////////////////////////////////////
+
+int GetSumNumbers(int m, int n)
+{
+    int sum = m;
+    if (n == m) return sum;
+    else
+    { 
+        sum = sum + GetSumNumbers(m + 1, n); 
+    }
+    return sum;
+}
